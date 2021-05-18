c4e9c345211119ab89c3218fe9b5b278b7955def
Daniil Gavrilovsky <gaaveer@gmail.com>
2021-05-18 13:29:35 +0300

    added free call

--- closer.go
+++ closer.go
@@ -16,6 +16,8 @@ type contextCloser struct {
 
 	errs     chan error
 	closeErr error
+
+	freeCh chan struct{}
 }
 
 // NewContextCloser creates new ContextCloser connected with context.Context.
@@ -28,6 +30,7 @@ func newContextCloser(ctx context.Context, c io.Closer) *contextCloser {
 		ctx:    closeCtx,
 		stop:   closeStop,
 		errs:   make(chan error, 1),
+		freeCh: make(chan struct{}),
 	}
 	go ctxCloser.observeCloserContext()
 
@@ -35,11 +38,31 @@ func newContextCloser(ctx context.Context, c io.Closer) *contextCloser {
 }
 
 func (w *contextCloser) observeCloserContext() {
-	<-w.ctx.Done()
-	w.errs <- w.Closer.Close()
+	select {
+	case <-w.ctx.Done():
+		w.errs <- w.Closer.Close()
+	case <-w.freeCh:
+		w.errs <- nil
+	}
+}
+
+// Free removes connection with context.
+// After this call Close or context cancel will not close underlying Closer.
+func (w *contextCloser) Free() error {
+	w.once.Do(func() {
+		close(w.freeCh)
+		w.closeErr = <-w.errs
+
+		// Always call context stop to free context.
+		// Call stop() after close(w.freeCh) because we do not want to close underlying Closer.
+		w.stop()
+	})
+
+	return w.closeErr
 }
 
 // Close closes underlying Closer.
+// After Free() call always returns nil.
 func (w *contextCloser) Close() error {
 	w.once.Do(func() {
 		w.stop()
